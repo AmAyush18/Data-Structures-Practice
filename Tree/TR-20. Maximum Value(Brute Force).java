905116679f534fd26b60b455159312beb83dac45
Ayush Dwivedi <91709845+AmAyush18@users.noreply.github.com>
2023-01-02 13:40:44 +0530

    Update TR-20. Maximum Value(Brute Force).java

--- Tree/TR-20. Maximum Value(Brute Force).java
+++ Tree/TR-20. Maximum Value(Brute Force).java
@@ -34,3 +34,32 @@ class Solution {
         dfs(node.right, level+1);
     }
 }
+
+// Brute Force Approach - 2 - Gives TLE
+class Solution {
+    ArrayList<Integer> ans;
+    ArrayList<Integer> maximumValue(Node node) {
+       ans = new ArrayList<>();
+       
+       helper(node, 0, ans);
+       
+       return ans;
+    }
+    
+    private void helper(Node node, int level, ArrayList<Integer> ans){
+        // base case
+        if(node == null){
+            return;
+        }
+        
+        if(ans.size() >= level+1){
+            int maxVal = Math.max(node.data, ans.get(level));
+            ans.set(level, maxVal);
+        }else{
+            ans.add(node.data);
+        }
+        
+        helper(node.left, level+1, ans);
+        helper(node.right, level+1, ans);
+    }
+}
