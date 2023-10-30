4307f4be6e3643f0995302848f5d1160125b821d
Ayush Dwivedi <91709845+AmAyush18@users.noreply.github.com>
2023-10-30 22:22:28 +0530

    Create 002. Insertion Sort.java

---
+++ Sorting/002. Insertion Sort.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        insertionSort(arr);
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+            int temp = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > temp) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = temp;
+        }
+    }
+}
