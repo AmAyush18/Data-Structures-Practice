a0317503fcf074e6ec8cb5e94755a163fc2bb66b
Ayush Dwivedi <91709845+AmAyush18@users.noreply.github.com>
2023-10-30 22:51:35 +0530

    Insertion Sorting

--- Sorting/003. InsertionSort.java
+++ Sorting/003. InsertionSort.java
@@ -21,7 +21,7 @@ class SelectionSort {
 
     public static void selectionSort(int[] arr) {
         int n = arr.length;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n-1; i++) {
             int min = i;
             for (int j = i + 1; j < n; j++) {
                 if (arr[j] < arr[min]) {
