b92a0cc6219265fdd5f874ee9fc9431a87cd5892
Ayush Dwivedi <91709845+AmAyush18@users.noreply.github.com>
2023-10-30 22:50:10 +0530

    InsertionSort

---
+++ Sorting/003. InsertionSort.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+// select smallest and put it first and keep moving the pointer
+class SelectionSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        selectionSort(arr);
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            int min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            if (min != i) {
+                swap(arr, min, i);
+            }
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+    }
+}
