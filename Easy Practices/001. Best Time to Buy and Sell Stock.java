145aa5cd46e28e743e566ea2ebe1227570a667cc
Ayush Dwivedi <91709845+AmAyush18@users.noreply.github.com>
2023-11-01 21:55:27 +0530

    Best Time to Buy and Sell Stock

---
+++ Easy Practices/001. Best Time to Buy and Sell Stock.java
@@ -0,0 +1,15 @@
+// Problem Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minSoFar = prices[0];
+        int maxProfit = 0;
+
+        for(int i = 1; i < prices.length; i++){
+            minSoFar = Math.min(minSoFar, prices[i]);
+            maxProfit = Math.max(maxProfit, prices[i] - minSoFar);
+        }
+
+        return maxProfit;
+    }
+}
